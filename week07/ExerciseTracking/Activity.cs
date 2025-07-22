e7724b1680e85e39b8284298468c56e491d4db31
Bryan Osorio <you@example.com>
2025-07-22 14:01:55 -0600

    Issues to fix: - Add missing Cycling.cs and Swimming.cs class files - Add missing calculations in Cycling and Swimming classes - Improve summary formatting (round values to 1 decimal place) - Refactor base class Activity as abstract to prevent direct instantiation - Remove unused virtual methods returning 0 (or abstract them instead)

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,31 @@
+public class Activity
+{
+    protected string _date;
+    protected int _minutes;
+
+    public Activity(string date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public virtual double GetDistance()
+    {
+        return 0;
+    }
+
+    public virtual double GetSpeed()
+    {
+        return 0;
+    }
+
+    public virtual double GetPace()
+    {
+        return 0;
+    }
+
+    public virtual string GetSummary()
+    {
+        return "";
+    }
+}
